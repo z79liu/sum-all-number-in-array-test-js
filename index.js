270385892e307a5a47681d9e8eb4fbe915cd33d5
Sam Meech-Ward <sam@meech-ward.com>
2017-10-06 23:06:31 -0700

    make the test pass

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  })
+  
+  return sum;
 }
 
 module.exports = sumItems;
