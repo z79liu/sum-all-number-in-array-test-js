a3486c59a7d0c3082d0e6430264b473632a6e8b1
Zeyu <haozai_03@hotmail.com>
2019-04-04 16:37:57 +0000

    recursion

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
   // Sum all the numbers in the array
-}
+  let x = 0
+  array.forEach(element => {
+    if (Array.isArray(element)) {
+      return x += sumItems(element)
+    }
+    else {
+        return x += element
+      }
+  })
+  return x;
+    
+  }
 
+console.log(sumItems([[1,2],1,2,4,5]))
 module.exports = sumItems;
